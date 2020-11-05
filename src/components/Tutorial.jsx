5d18490229b537ce43f719157e877315039ba463
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-05 15:59:38 +0000

    refactored into react styled components

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import {Link} from '@reach/router'
+import styled from 'styled-components'
+
+const TutorialContainer = styled.div`
+margin: 0% auto;
+
+  width: 80%;
+  height: 550px;
+  display: grid;
+  grid-template-rows: 20% 80%;
+  grid-row-gap: 25px;
+  color: white;
+  opacity: 90%;
+`
+
+const TutorialHeader = styled.span`
+font-family: 'Press Start 2P', cursive;
+  font-size: 2.5rem;
+  margin-top: 4%;
+`
+
+const TutorialHowToPlayContainer = styled.div`
+width: 80%;
+  height: 350px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: 90% 10%;
+  font-family: 'VT323', monospace;
+  font-size: 1.5rem;
+`
+const TutorialPlayButton = styled.button`
+border-radius: 10px;
+  width: 100px;
+  height: 30px;
+`
 
 const Tutorial = (props) => {
 
   
   
     return (
-      <div className='tutorial-container'>
-        <span className='tutorial-header'>Tutorial</span>
-        <div className='tutorial-howToPlay-container'>
+      <TutorialContainer >
+        <TutorialHeader>Tutorial</TutorialHeader>
+        <TutorialHowToPlayContainer>
           
           <p className='tutorial-paragraph'>Welcome {props.username}!</p>
           <Link to='/game'>
-          <button className='tutorial-playButton'>PLAY!</button>
+          <TutorialPlayButton>PLAY!</TutorialPlayButton>
           </Link>
-         </div>
-      </div>
+         </TutorialHowToPlayContainer>
+      </TutorialContainer>
     );
   
 }
--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import {Link} from '@reach/router'
 import helpIcon from '../assets/help-icon.png'
+import styled from 'styled-components'
 
+const HomeTitle = styled.span`
+font-family: 'Press Start 2P', cursive;
+  font-size: 2.5rem;
+  color: white;
+  display: flex;
+  flex-direction: column;
+`
+
+const InfoDiv = styled.div`
+ margin-top: 30%;
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+`
 
 class Username extends Component {
 
@@ -18,10 +32,10 @@ class Username extends Component {
       <div className='username-container'>
         
         
-        <span className='home-title'>NC HELPER!</span>
-        <span className='home-moto'>A GAME ABOUT KINDNESS</span>
+        <HomeTitle>NC HELPER!</HomeTitle>
+        <HomeTitle>A GAME ABOUT KINDNESS</HomeTitle>
         <i class="fas fa-medkit"></i>
-        <div className='info-container'>
+        <InfoDiv>
         <span>Enter username: </span>
         <form onChange={this.handleSubmit}>
         <input type='text' name='username'  ></input>
@@ -29,7 +43,7 @@ class Username extends Component {
         <Link to='/tutorial'>
         <input type='submit'></input>
         </Link>
-        </div>
+        </InfoDiv>
       </div>
     );
   }
