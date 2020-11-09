13cb6341bab0967a0036598dbc1c1dcd16657680
Tibs088 <tibs088@gmail.com>
2020-11-09 11:19:28 +0000

    upon finishing the game by getting the max score, will navigate to new page and destroy instance of Phaser

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import './App.css';
 import Game from './game.jsx';
-import {Router} from '@reach/router'
+import { Router } from '@reach/router'
 import Username from './components/Username';
-import React,{Component} from 'react'
+import React, { Component } from 'react';
+import WinPage from './pages/WinPage';
 
 class App extends Component {
 
-state = {
-  user: ''
-}
-
-setUser = (username) => {
-     this.setState({user: username})
+  state = {
+    user: ''
   }
 
-  render() { 
+  setUser = (username) => {
+    this.setState({ user: username })
+  }
+
+  render() {
     return (
-    <div className="App">
-      <Router>
-      <Username path='/' setUser={this.setUser} />
-      <Game path='/game' name={this.state.user}/>
-      </Router>
-    </div>
+      <div className="App">
+        <Router>
+          <Username path='/' setUser={this.setUser} />
+          <Game path='/game' name={this.state.user} />
+          <WinPage path='/end' />
+        </Router>
+      </div>
     )
-}
+  }
 }
 
 export default App;
