abb22813142c89bdcf265285030d0549dc9877ae
CharlyMannion <charlottemannion100@gmail.com>
2020-11-09 14:49:34 +0000

    adds error handling to win page

--- src/pages/WinPage.jsx
+++ src/pages/WinPage.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Loader from "../components/Loader";
+import ErrorDisplay from "../components/ErrorDisplay";
 
 class WinPage extends Component {
   state = {
-    user: '',
-    users: []
-  }
-
-
+    user: "",
+    users: [],
+    isLoading: true,
+  };
 
   // postUser = (username) => {
   //   console.log('post user')
@@ -26,27 +27,49 @@ class WinPage extends Component {
   // }
 
   getUsers = () => {
-    console.log('get user')
-    fetch('https://helpers-game-backend.herokuapp.com/playerlist', {
-      method: 'GET',
+    console.log("get user");
+    fetch("https://helpers-game-backend.herokuapp.com/playerlist", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-    .then((res) => res.json())
-    .then((players) => {
-      console.log(players)
-    })
-  }
+      .then((res) => res.json())
+      .then((players) => {
+        console.log(players);
+        this.setState({ users: players, isLoading: false });
+        console.log(this.state.users, "STATE");
+      })
+      .catch(({response}) => {
+        this.setState({
+            error: {
+              response
+              // status: response.status,
+              // message: response.data.msg,
+            }
+        })
+    });
+  };
 
   componentDidMount() {
     this.getUsers();
   }
 
   render() {
+    const { users, isLoading, error } = this.state;
+    if (error) return <ErrorDisplay {...error} />;
+    if (isLoading) return <Loader />;
+
     return (
-      <h1>Well Done!</h1>
-    )
+      <div className="user-list">
+        <h1>Well Done!</h1>
+        <ul>
+          {users.map((user) => {
+            return <li>{user.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
