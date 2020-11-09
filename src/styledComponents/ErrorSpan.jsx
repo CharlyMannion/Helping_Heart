a8d049f3cda82f2a42c9e827081d49cd60dc8e07
CharlyMannion <charlottemannion100@gmail.com>
2020-11-09 16:23:35 +0000

    extracts styled components from errordisplay and username

---
+++ src/styledComponents/ErrorSpan.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const ErrorSpan = styled.span`
+font-family: 'Press Start 2P', cursive;
+  color: white;
+  text-decoration: none;
+`
+
+export default ErrorSpan;
