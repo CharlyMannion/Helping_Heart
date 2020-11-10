6626ffa4c2c6327b46e9b320f6c57705d22f280e
CharlyMannion <charlottemannion100@gmail.com>
2020-11-10 16:35:17 +0000

    adds instructions to tutorial page

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "@reach/router";
 import TutorialContainer from "../styledComponents/TutorialContainer";
-import TutorialHeader from "../styledComponents/TutorialHeader";
 import StyledParagraph from "../styledComponents/StyledParagraph";
 import StyledButton from "../styledComponents/StyledButton";
 import StyledHeader from "../styledComponents/StyledHeader";
@@ -12,15 +11,12 @@ const Tutorial = (props) => {
       <StyledHeader>Tutorial</StyledHeader>
       <StyledParagraph>
         <p>Welcome {props.username}!</p>
-        <p>Some info on controls</p>
+        <p>Run around the map and find those in need.</p>
         <p></p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
-        <p>Some info on controls</p>
+        <p>Use W,A,S,D keys to move around.</p>
+        <p>When you see a person, hit the space bar to chat to them.</p>
+        <p>Help 5 of your new pals to complete the game.</p>
+        <p></p>
         <Link to="/game">
           <StyledButton>PLAY!</StyledButton>
         </Link>
