40f6d86117121422bac50ecb786fde4ec9ab273b
SarahBrowncross <sarah.browncross@gmail.com>
2020-11-10 16:56:38 +0000

    lonely story added

--- src/Game_Components/scenario_tree.js
+++ src/Game_Components/scenario_tree.js
@@ -4,14 +4,23 @@ const scenarioTree = {
 	'lookDog': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Where do you look?', zone, 'tree', 'pond', 'tree', 'pond', false).setScrollFactor(0)},
 	'tree': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'You find the dog', zone, 'continue', '', null, null, true).setScrollFactor(0)},
 	'pond': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "It's a dog, not a duck", zone, 'continue', '', 'lookDog', false).setScrollFactor(0)},
-	'startWoodNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Will you help Frank?', zone, 'Yes', 'No', 'lonely', null, false).setScrollFactor(0)},
-	'lonely': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "I haven't seen anybody in weeks, will you chat to me?", zone, 'Yes', 'No', 'chat', null, false).setScrollFactor(0)},
-	'chat': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "What do you chat to Frank about?", zone, 'Your rock collection', 'Covid', 'rocks', 'covid', false).setScrollFactor(0)},
-	'rocks': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "Frank likes rocks, he's cheered right up", zone, 'continue', '', null, null, true).setScrollFactor(0)},
-	'covid': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "Nobody likes covid. Frank is still sad", zone, 'continue', '', null, null, false).setScrollFactor(0)},
+	
+	'startWoodNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "Hey, it's been weeks since I spoke to anyone. Have you got time for a chat?", zone, 'Yes', "No, I'm busy helping people", 'lonely', null, false).setScrollFactor(0)},
+	'lonely': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "*Frank talks about his problems for 20 minutes*", zone, 'Tell me more', "Sorry, I've got to go", 'chat', null, false).setScrollFactor(0)},
+	'chat': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "You're such a great listener. Do you have any advice for me?", zone, 'Frank, you know what to do', 'Always wear sunscreen', 'answerWithin', 'sunscreen', false).setScrollFactor(0)},
+	'answerWithin': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "You're right! My heart knew the right path all along!", zone, 'continue', '', null, null, true).setScrollFactor(0)},
+	'sunscreen': function(zone) {this.createDialog(this, 2243.10344827586, 4050, "Umm, okay. I'll go to the shops and buy some. *Frank leaves, confused*", zone, 'continue', '', null, null, false).setScrollFactor(0)},
+	
 	'startParkNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Will you help me pick flowers?', zone, 'Yes', 'No', null, null, false).setScrollFactor(0)},
+	
+	
+	
+	
+	
 	'startSupermarketNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Will you help me find the soup?', zone, 'Yes', 'No', null, null, false).setScrollFactor(0)},
+	
 	'startOldForestNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Will you help me chop some wood?', zone, 'Yes', 'No', null, null, false).setScrollFactor(0)},
+	
 	'startGraveyardNPC': function(zone) {this.createDialog(this, 2243.10344827586, 4050, 'Will you help me fight this ghost?', zone, 'Yes', 'No', null, null, false).setScrollFactor(0)},
   }
 
